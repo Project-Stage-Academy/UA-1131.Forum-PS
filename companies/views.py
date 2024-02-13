93921f6be7f7d2995dc9aeb628f4e05e163487f1
Yan Zhylavy <yanzhylavy@gmail.com>
2024-02-13 22:55:46 +0200

    Added permission_classes to the companies endpoints

--- companies/views.py
+++ companies/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from .models import Companies
 from .serializers import CompaniesSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class CompaniesListCreateView(generics.ListCreateAPIView):
     queryset = Companies.objects.all()
     serializer_class = CompaniesSerializer
+    permission_classes = (IsAuthenticated,)
 
 
 class CompaniesRetrieveUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Companies.objects.all()
     serializer_class = CompaniesSerializer
+    permission_classes = (IsAuthenticated,)
