15f530c6eab0f992ba834de75762333f5d931ec1
Yan Zhylavy <yanzhylavy@gmail.com>
2024-02-14 12:33:43 +0200

    Added tests for companies endpoints

--- companies/tests.py
+++ companies/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIClient
+from authentication.models import CustomUser
 
-# Create your tests here.
+class CreateCompanyTest(APITestCase):
+    def test_create_company(self):
+        user = CustomUser()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        response = client.post('http://127.0.0.1:8000/companies/',{'brand': 'test_brand', 'is_registered': True}, format='json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_company(self):
+        user = CustomUser()
+        client = APIClient()
+        client.force_authenticate(user=user)
+        client.post('http://127.0.0.1:8000/companies/',{'brand': 'test_brand', 'is_registered': True}, format='json')
+        response = client.get('http://127.0.0.1:8000/companies/2', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_negative_unauthorized_user(self):
+        response = self.client.post('http://127.0.0.1:8000/companies/',{'brand': 'test_brand', 'is_registered': True}, format='json')
+        self.assertEqual(response.status_code, 401)
