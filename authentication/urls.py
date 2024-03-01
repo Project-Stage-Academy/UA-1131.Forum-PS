28e3a36bdc6b764ed975641e84beb86c31a02e96
parsival2022 <99756579+parsival2022@users.noreply.github.com>
2024-03-01 20:02:13 +0200

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from authentication import views
 
 urlpatterns = [
     path('register/', views.UserRegistrationView.as_view(), name="auth_register"),
+    path('relate/', views.RelateUserToCompany.as_view(), name="relate"),
     path('email_verify/', views.VerifyEmail.as_view(), name="email_verify"),
     path('users/<int:pk>/', views.UserUpdateView.as_view(), name="user_details"),
     path('users/<int:pk>/password_update/', views.UserPasswordUpdateView.as_view(), name="user_pass_update"),
