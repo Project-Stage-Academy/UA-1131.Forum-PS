105b232a4e611570740ec391025f70c1e6ff78ac
Yan Zhylavy <yanzhylavy@gmail.com>
2024-03-07 15:27:59 +0200

    Added model for company articles

--- companies/models.py
+++ companies/models.py
@@ -1,9 +1,17 @@
+from authentication.models import CustomUser, Company, CompanyAndUserRelation
 from django.db import models
 
-from authentication.models import CustomUser, Company
 class Subscription(models.Model):
     subscription_id = models.BigAutoField(primary_key=True)
     investor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     get_email_newsletter = models.BooleanField(default=False)
     subscribed_at = models.DateTimeField(auto_now_add=True)
+
+
+class CompanyArticle(models.Model):
+    article_id = models.BigAutoField(primary_key=True)
+    relation = models.ForeignKey(CompanyAndUserRelation, on_delete=models.CASCADE)
+    article_text = models.TextField(null=True)
+    article_tags = models.TextField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True)
