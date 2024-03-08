cf44b25784716fd6946758fff7d07740e84f3b54
Arseniy Kostromin <arseniy.kostromin03@gmail.com>
2024-03-08 12:46:44 +0200

    remove old app

--- forum/settings.py
+++ forum/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
     'authentication',
-    'password_recovery',
     'companies',
     'chats',
     'livechats',
