f491828b27780fbb33b20545eab6dd81da293cd0
Parcival <ipavlenko239@gmail.com>
2024-03-09 12:49:26 +0200

    models changed

--- companies/schemas.py
+++ companies/schemas.py
@@ -1,10 +1,15 @@
 from datetime import datetime
 from pydantic import BaseModel, Field
+from typing import List
 
 
-class CompanyArticle(BaseModel):
+class Article(BaseModel):
     relation: int
     article_text: str
     article_tags: str
     created_at: str = Field(
         default_factory=lambda: datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    
+class CompanyArticles(BaseModel):
+    company_id:int
+    aricles:List[Article]
