ea9ab2b4529e51ca52ee3b1b3b3bbd35fbaaaf88
Yan Zhylavy <yanzhylavy@gmail.com>
2024-03-10 21:30:43 +0200

    Added field 'article_title' into article model

--- companies/schemas.py
+++ companies/schemas.py
@@ -7,6 +7,7 @@ from typing import List
 class Article(BaseModel):
     article_id:str = Field(default_factory=lambda:str(ObjectId()))
     relation: int
+    article_title:str
     article_text: str
     article_tags: str
     created_at: str = Field(
