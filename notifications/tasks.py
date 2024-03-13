9f9f14ed01ac078f0ff98e617b3f42538899c3ef
arsemon4ik <misterarsemon4ik@gmail.com>
2024-03-13 14:55:56 +0200

    add new methods to tasks

--- notifications/tasks.py
+++ notifications/tasks.py
@@ -11,6 +11,19 @@ def send_password_update_notification(user):
 def send_password_reset_notification(email, reset_link):
     EmailAuthenticationManager.send_password_reset_notification(email, reset_link)
 
+@shared_task
+def send_article_notification(company_id, users):
+    EmailNotificationManager.send_article_notification(company_id, users)
+
+
+@shared_task
+def send_subscribe_notification(company_id):
+    EmailNotificationManager.send_subscribe_notification(company_id)
+
+
+@shared_task
+def send_message_notification(user):
+    EmailNotificationManager.send_message_notification(user)
 
 @shared_task
 def create_notification(data):
